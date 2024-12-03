050cd14518cc800efc59a5b4d547494239ee1543
Cécile Duffaut <cecileduffaut21@gmail.com>
2024-12-03 23:10:57 +0100

    On veut notre bac

--- utils/utils.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-type Movie struct {
-	MovieName       string `json:"movie_name" validate:"required"`
-	ClientName      string `json:"client_name" validate:"required"`
-	ClientFirstName string `json:"client_firstname" validate:"required"`
-	ClientMail      string `json:"client_mail" validate:"required,email"`
-}
